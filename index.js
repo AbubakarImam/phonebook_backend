9751ffbda9076509abe5485d4198a3f9184c0ebc
AbubakarImam <salamabubakarimam@gmail.com>
2023-10-27 17:05:26 +0100

    Fix port

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong')
 })
 
-const PORT = 3002
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
